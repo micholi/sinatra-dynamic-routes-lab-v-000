187bd033ed1a7ebb368c75556968e9291be82736
Michelle Olivieri <michelleo72@gmail.com>
2018-05-10 15:45:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,4 +8,24 @@ class App < Sinatra::Base
     # "Goodbye, #{@user_name}."
   end
 
+  get 'square/:number'
+    # @user_name = params[:name]
+    # "Goodbye, #{@user_name}."
+  end
+
+  get 'say/:number/:phrase'
+    # @user_name = params[:name]
+    # "Goodbye, #{@user_name}."
+  end
+
+  get 'say/:word1/:word2'
+    # @user_name = params[:name]
+    # "Goodbye, #{@user_name}."
+  end
+
+  get ':operation/:number1/:number2'
+    # @user_name = params[:name]
+    # "Goodbye, #{@user_name}."
+  end
+
 end
